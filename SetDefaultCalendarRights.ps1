459599ffb263d3dad3060d06a89e6613290b314c
Jaap de Koning <jaap.dekoning@gmail.com>
2018-08-12 11:50:26 +0200

    added some comments

--- SetDefaultCalendarRights.ps1
+++ SetDefaultCalendarRights.ps1
@@ -4,6 +4,21 @@ function Set-DefaultCalendarRights {
         [ValidateSet("AvailabilityOnly","LimitedDetails")][string]$AccessRights
     )
 
+    <#
+    .SYNOPSiS
+    Sets the input mailboxes default calendar rights
+
+    .PARAMETER Mailboxes
+    Specifies either a single or array of mailbox UPNs
+
+    .PARAMETER AccessRights
+    Specifies the value the calendar should be set to.
+
+    .EXAMPLE
+    Set-DefaultCalendarRights -Mailboxes (Get-Mailbox | Select UserPrincipalName) -AccessRights LimitedDetails
+
+    #>
+
     Foreach ($Mailbox in $Mailboxes) {
         $CalendarFolder = Get-MailboxFolderStatistics $Mailbox | Where-Object {$_.FolderType -eq "Calendar"}
         if ($CalendarFolder) {
@@ -13,7 +28,7 @@ function Set-DefaultCalendarRights {
     }
 }
 
-# Examples
+# Example: load a set of mailboxes and set the access rights.
 
 $Mailboxes = get-mailbox -OrganizationalUnit "OU=Users,DC=jaaplab,DC=nl"
 
